872f76ca543df3264e77695a9d715a80b29751a9
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-08-05 11:24:46 +0000

    Add product model

--- product/models.py
+++ product/models.py
@@ -9,3 +9,21 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+
+
+class Product(models.Model):
+    main_image = CloudinaryField('image', default='placeholderProduct')
+    item_name = models.CharField(max_length=50, unique=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
+    description = models.TextField(
+        default='Contact us to find out more about this gown',
+        blank=True,
+        max_length=2000)
+    colours = models.TextField(default='Ivory', blank=True, max_length=500)
+    sizes_avail = models.TextField(
+        default='8 - 26', blank=True, max_length=500)
+    likes = models.ManyToManyField(User, related_name='fav_products')
+
+    def __str__(self):
+        return self.item_name
