b9fec283c5ab07303f0f4530a9134a081326674d
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-08-05 11:13:06 +0000

    Add category model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    category_image = CloudinaryField('image', default='placeholderCategory')
+    category_name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.category_name
