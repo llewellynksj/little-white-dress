d1ba4861542272c82c9547cf3098acf5f1fcef4d
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-08-06 14:02:50 +0000

    Add edit account settings form and template

--- customer/forms.py
+++ customer/forms.py
@@ -4,6 +4,18 @@ from django import forms
 # from .models import Customer
 
 
+class EditAccountSettingsForm(UserChangeForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    telephone = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+
 class UpdatePasswordForm(PasswordChangeForm):
     old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password'}))
     new_password1 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password'}))
--- customer/views.py
+++ customer/views.py
@@ -4,11 +4,20 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import RegistrationForm, UpdatePasswordForm
+from .forms import RegistrationForm, UpdatePasswordForm, EditAccountSettingsForm
 # EditProfileForm, PasswordUpdateForm, CreateProfileForm
 from django.contrib.auth.models import User
 # from .models import Customer
 from django.http import HttpResponseRedirect
+
+
+class AccountSettings(generic.UpdateView):
+    form_class = EditAccountSettingsForm
+    template_name = 'registration/account_settings.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
 
 
 class UpdatePassword(PasswordChangeView):
