868c356f9bbb35defd81df20d0c041e2b48032d6
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-08-06 13:20:35 +0000

    Add product_details.html and link with url and view to display individual product details

--- product/urls.py
+++ product/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
     path('', views.CategoryList.as_view(), name='index'),
     path('products/', views.ProductList.as_view(), name='products'),
     path(
-        'category/<str:selected_cat>/', views.CategoryProducts, name='category'
-        ),
+        'category/<str:selected_cat>/',
+        views.CategoryProducts,
+        name='category'),
+    path(
+        'product/<int:pk>',
+        views.ProductDetails.as_view(),
+        name='product_details'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Category, Product
+
+
+class ProductDetails(generic.DetailView):
+    # Displays individual product details
+    model = Product
+    template_name = 'product_details.html'
+
+    # Code adapted from Codemy 'Add Blog Posts to Django' tutorial:
+    # https://bit.ly/3s2sUq4
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetails, self).get_context_data(*args, **kwargs)
+        data = get_object_or_404(Product, id=self.kwargs['pk'])
+        liked = False
+        if data.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        context['liked'] = liked
+        return context
 
 
 def CategoryProducts(request, selected_cat):
