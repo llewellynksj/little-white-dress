21669d552d980b279a858eec8c5442fd5b6f813b
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-08-06 12:02:34 +0000

    Add category_products.html and link with url and view to display products within selected category

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.CategoryList.as_view(), name='index'),
     path('products/', views.ProductList.as_view(), name='products'),
+    path(
+        'category/<str:selected_cat>/', views.CategoryProducts, name='category'
+        ),
 ]
--- product/views.py
+++ product/views.py
@@ -3,12 +3,23 @@ from django.views import generic
 from .models import Category, Product
 
 
+def CategoryProducts(request, selected_cat):
+    # Displays all products within the selected category
+    category_products = Product.objects.filter(
+        category__category_name=selected_cat)
+    return render(
+        request,
+        'category_products.html',
+        {'selected_cat': selected_cat, 'category_products': category_products})
+
+
+class ProductList(generic.ListView):
+    # Displays all products when 'all styles' is selected
+    model = Product
+    template_name = 'all_products.html'
+
+
 class CategoryList(generic.ListView):
     # Displays the product categorys on the homepage
     model = Category
     template_name = 'index.html'
-
-
-class ProductList(generic.ListView):
-    model = Product
-    template_name = 'all_products.html'
