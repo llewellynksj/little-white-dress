5553976c4bcfc16c530ad0eb7ff59e7949d51917
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-08-23 14:39:42 +0000

    Amend fields in recommendation model

--- recommendations/forms.py
+++ recommendations/forms.py
@@ -6,12 +6,12 @@ from .models import Recommendation
 class AddNewRecommendationForm(forms.ModelForm):
     class Meta:
         model = Recommendation
-        fields = ('type_of_recommendation', 'title', 'location', 'link', 'summary', 'posted_by',)
+        fields = ('type_of_recommendation', 'title', 'location', 'web_link', 'summary', 'posted_by',)
         widgets = {
             'type_of_recommendation': forms.Select(attrs={'class': 'form-control', 'type': 'select'}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'location': forms.TextInput(attrs={'class': 'form-control'}),
-            'link': forms.URLInput(attrs={'class': 'form-control', 'type': 'url'}),
+            'web_link': forms.TextInput(attrs={'class': 'form-control'}),
             'summary': forms.TextInput(attrs={'class': 'form-control'}),
             # 'date_posted': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'posted_by': forms.TextInput(attrs={'class': 'form-control'}),
--- recommendations/models.py
+++ recommendations/models.py
@@ -19,10 +19,10 @@ class Recommendation(models.Model):
     type_of_recommendation = models.CharField(max_length=100, choices=TYPES)
     title = models.CharField(max_length=60)
     location = models.CharField(max_length=50, help_text='Please state the city or town')
-    link = models.URLField()
+    web_link = models.TextField(max_length=2000, default='www.google.com')
     summary = models.TextField(max_length=500, help_text="Tell us a bit about why you're recommending this service")
     date_posted = models.DateField(auto_now=True)
-    posted_by = models.CharField(max_length=50, help_text='Enter your name, a username, or anonymous', default='{{ user.first_name }}')
+    posted_by = models.CharField(max_length=50, help_text='Enter your name, a username, or anonymous')
 
     def __str__(self):
         return self.title
