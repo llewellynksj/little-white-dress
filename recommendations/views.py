fdc813e1a0610b56768e0f207bae8628e3bac30a
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-08-23 11:24:40 +0000

    Add view and html code to display users recommendations

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 from django.views import generic
 from .models import Recommendation
 from .forms import AddNewRecommendationForm
@@ -20,25 +22,32 @@ class AddRecommendations(generic.CreateView):
         return super().form_valid(form)
 
     def get_success_url(self) -> str:
-        return reverse_lazy('community', kwargs={'pk': self.object.pk})
+        return reverse_lazy('my_recommendations', kwargs={'pk': self.object.pk})
 
 
 class MyRecommendationsList(generic.ListView):
-    """
-    Displays all recommendation posts by User
-    """
+#     """
+#     Displays all recommendation posts by User
+#     """
     model = Recommendation
     template_name = 'my_recommendations.html'
 
+    # code from Codemy 'Create a Blog Profile Page' Video:
+    # http://bit.ly/3OsUgy8:
+    def get_context_data(self, *args, **kwargs):
+        context = super(
+            MyRecommendationsList, self).get_context_data(
+                *args, **kwargs)
+        user_recommendations = Recommendation.objects.all
+        context['user_recommendations'] = user_recommendations
+        return context
+    
     # Make the user id available to be able to be saved to the form
     # Code from Codemy 'Profile Account Creation - Django Blog #32' video:
     # https://bit.ly/44Ymcjg
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
-
-    def get_success_url(self) -> str:
-        return reverse_lazy('community', kwargs={'pk': self.object.pk})
+    # def form_valid(self, form):
+    #     form.instance.user = self.request.user
+    #     return super().form_valid(form)
 
 
 class RecommendationList(generic.ListView):
