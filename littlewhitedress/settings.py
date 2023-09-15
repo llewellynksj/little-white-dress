79731202386a1771f521bfe4dac0e19abaeb4943
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-09-15 14:20:17 +0000

    Fix syntax error in settings

--- littlewhitedress/settings.py
+++ littlewhitedress/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-llewellynks-littlewhite-hbfm93ebiwl.ws-eu104.gitpod.io',
