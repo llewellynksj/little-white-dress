df5ec315e1e4864121191ae631d1f9cb1e95c25c
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-09-21 17:14:07 +0000

    Add opening hours to contact details model and display on contact page

--- contact/models.py
+++ contact/models.py
@@ -7,14 +7,48 @@ class ContactDetail(models.Model):
     ContactDetail model allows admin to add and
     update store address, phone number and email address
     """
-    first_line_of_address = models.CharField(max_length=200)
+    first_line_of_address = models.CharField(
+        max_length=200)
     second_line_address = models.CharField(
-        max_length=200, default='Enter second line of address')
-    city = models.CharField(max_length=50)
-    county = models.CharField(max_length=50)
-    postcode = models.CharField(max_length=15)
+        max_length=200,
+        default='Enter second line of address')
+    city = models.CharField(
+        max_length=50)
+    county = models.CharField(
+        max_length=50)
+    postcode = models.CharField(
+        max_length=15)
     telephone = models.PositiveBigIntegerField()
-    email = models.EmailField(validators=[EmailValidator])
+    email = models.EmailField(
+        validators=[EmailValidator])
+    monday_opening_hrs = models.CharField(
+        max_length=1000,
+        blank=True,
+        null=True)
+    tuesday_opening_hrs = models.CharField(
+        max_length=1000,
+        blank=True,
+        null=True)
+    wednesday_opening_hrs = models.CharField(
+        max_length=1000,
+        blank=True,
+        null=True)
+    thursday_opening_hrs = models.CharField(
+        max_length=1000,
+        blank=True,
+        null=True)
+    friday_opening_hrs = models.CharField(
+        max_length=1000,
+        blank=True,
+        null=True)
+    saturday_opening_hrs = models.CharField(
+        max_length=1000,
+        blank=True,
+        null=True)
+    sunday_opening_hrs = models.CharField(
+        max_length=1000,
+        blank=True,
+        null=True)
 
     def __str__(self):
         return self.first_line_of_address
