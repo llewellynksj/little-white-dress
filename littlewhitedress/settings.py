f3ea8e30423636e72dd64ecb38b847a2ae044eaf
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-09-22 08:27:51 +0000

    Amend typo in send mail settings

--- littlewhitedress/settings.py
+++ littlewhitedress/settings.py
@@ -163,8 +163,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Send Mail Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('SEND_MAIL_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('SEND_MAIL_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
