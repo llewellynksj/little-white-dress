fc53866932a001df508e17f5d582f4446e7bd5f8
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-09-23 10:07:15 +0000

    Add test for appointment database

--- appointment/tests.py
+++ appointment/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Appointment
 
-# Create your tests here.
+
+class AppointmentTests(TestCase):
+    """
+    Test Appointment Database
+    """
+    @classmethod
+    def setUpTestData(cls):
+        """
+        Create test database
+        """
+        cls.appointment = Appointment.objects.create(
+            booking_date='2023-12-18',
+            time='09:00',
+            no_in_party='3',
+            )
+
+    def test_model_content(self):
+        """
+        Test objects successfully created
+        """
+        self.assertEqual(self.appointment.booking_date, '2023-12-18')
+        self.assertEqual(self.appointment.time, '09:00')
+        self.assertEqual(self.appointment.no_in_party, '3')
