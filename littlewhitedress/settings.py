3bcda5426a4693135c3e2881a55e62bfd9a538a4
Kate L <llewellyn.ksj@gmail.com>
2025-02-05 18:43:39 +0000

    Remove vercel allowed host

--- littlewhitedress/settings.py
+++ littlewhitedress/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
-    'little-white-dress.vercel.app',
     '8000-llewellynks-littlewhite-hbfm93ebiwl.ws-eu104.gitpod.io',
     'localhost',
     'little-white-dress.herokuapp.com',
